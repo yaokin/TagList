4c2c73ed82c9e05e3263307fd92ae8f4a09b4c95
加藤八起 <katouyaoki@katouhakkinoMacBook-Pro.local>
2019-05-15 20:01:01 +0900

    first commit

---
+++ SubTableViewController.swift
@@ -0,0 +1,106 @@
+//
+//  SubTableViewController.swift
+//  list
+//
+//  Created by 加藤八起 on 2019/05/15.
+//  Copyright © 2019 加藤八起. All rights reserved.
+//
+
+import UIKit
+
+class SubTableViewController: UITableViewController {
+
+    //セルに表示するデータ
+    let memo = [
+        (name:"A",url:"B"),
+        (name:"C",url:"D"),
+        (name:"E",url:"F"),
+        (name:"G",url:"H")
+    ]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    //セクションの個数
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    //セクションの行数
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        //配列 memo の値の個数
+        return memo.count
+    }
+    
+    //セルを作る
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //テーブルのセルを参照する
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        //テーブルに memo のデータを表示する
+        let memodata = memo[(indexPath as NSIndexPath).row]
+        cell.textLabel?.text = memodata.name
+        cell.detailTextLabel?.text = memodata.url
+        // Configure the cell...
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+    //セグエで移動する前にデータを受け渡す
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+ 
+
+}
---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  list
+//
+//  Created by 加藤八起 on 2019/05/15.
+//  Copyright © 2019 加藤八起. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
